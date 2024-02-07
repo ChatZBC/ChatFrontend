92500361e9e0303e13c5400395067cf8d5f602a8
Pandaa <TuePVM@gmail.com>
2024-02-07 13:44:15 +0100

    WIP

--- src/app/services/data.service.spec.ts
+++ src/app/services/data.service.spec.ts
@@ -1,16 +1,19 @@
-import { TestBed } from '@angular/core/testing';
+// import { TestBed } from '@angular/core/testing';
 
-import { DataService } from './data.service';
+// import * as SignalR from '@microsoft/signalr'
+// import { DataService } from './data.service';
 
-describe('DataService', () => {
-  let service: DataService;
+// describe('DataService', () => {
+//   let service: DataService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(DataService);
-  });
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({});
+//     service = TestBed.inject(DataService);
+//   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+//   it('should be created', () => {
+//     expect(service).toBeTruthy();
+//   });
+
+//   it('')
+// });
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
 
+import * as SignalR from '@microsoft/signalr'
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  public data: any;
+  private hubConnection : signalR.HubConnection
+    public startConnection = () => {
+      this.hubConnection = new SignalR.HubConnectionBuilder()
+                                  .withUrl('https://localhost:5001')
+                                  .build();
+      this.hubConnection
+        .start()
+        .then(() => console.log('Connection started'))
+        .catch(err => console.log('Error while starting connection' + err))
+    }
 
-  constructor() { }
+    public sendMessage = () => {
+      this.hubConnection.on('Message',(data) => {
+        this.data = data;
+        console.log(data)
+      })
+    }
 }
